a46fb46ea0ca65d2974c1f3c59025b3027b4221a
Jim Saari <jim.saari97@gmail.com>
2020-12-03 22:20:28 +0200

    tweak widget type to accept string & number union

--- src/types/widgets.ts
+++ src/types/widgets.ts
@@ -1,4 +1,4 @@
-type WidgetType = string | Widget | string[] | number[] | Widget[];
+type WidgetType = string | Widget | string[] | number[] | Widget[] | (string | number)[];
 
 export interface Widget {
   name: string;
